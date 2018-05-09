b6c8727e44730292131b32846fce389abc4523b4
Rui Figueiredo <ruimgf95@gmail.com>
2018-05-09 12:20:02 +0100

    correct sintax error setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
      version='0.1',    # Update the version number for new releases
      packages=find_packages(exclude=['tests*','images','etcs']),
      description='people detection python package',
-     install_requires=['argparse','matplotlib','scipy','tqdm','requests','fire','dill','scipy','scikit-learn','opencv-python','h5py','Pillow','requests','psutil']
+     install_requires=['argparse','matplotlib','scipy','tqdm','requests','fire','dill','scipy','scikit-learn','opencv-python','h5py','Pillow','requests','psutil'],
      data_files=[('facenet/align',['det1.npy','det2.npy','det3.npy'])]
 )
